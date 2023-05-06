0bf92a9d32a32037639a73279fd1a8b822c6d764
Boey01 <boeyshuwei@gmail.com>
2023-05-06 13:49:10 +0800

    installing react app, reconstruct frontend file

--- IWDAT/IWDAT/urls.py
+++ IWDAT/IWDAT/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from frontend.views import index
+from frontend.django.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
