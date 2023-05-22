2b7e7e8fc87e5b919fa182e5ca84d196347ccb2d
Boey01 <boeyshuwei@gmail.com>
2023-05-22 22:49:54 +0800

    Editable Cells

--- IWDAT/frontend/react-folder/src/components/TableRenderer.js
+++ IWDAT/frontend/react-folder/src/components/TableRenderer.js
@@ -6,16 +6,15 @@ import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
-import { styled,  useTheme } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import TablePagination from "@mui/material/TablePagination";
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
 
 const CustomTableContainer = styled(TableContainer)({
   maxHeight: "60vh",
@@ -39,7 +38,7 @@ export const TableRenderer = ({ sheetData }) => {
     });
   });
 
-  const rows = sheetData[sheetName[0]];
+  const [rows, setRows] = useState(sheetData[sheetName[0]]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
@@ -50,6 +49,14 @@ export const TableRenderer = ({ sheetData }) => {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
+  };
+
+  const handleCellValueChange = (newValue, rowIndex, field) => {
+    setRows((prevRows) => {
+      const updatedRows = [...prevRows];
+      updatedRows[rowIndex][field] = newValue;
+      return updatedRows;
+    });
   };
 
   const emptyRows =
@@ -70,10 +77,16 @@ export const TableRenderer = ({ sheetData }) => {
             {(rowsPerPage > 0
               ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               : rows
-            ).map((row, index) => (
-              <TableRow key={index}>
+            ).map((row, rowIndex) => (
+              <TableRow key={rowIndex}>
                 {columns.map((column) => (
-                  <TableCell key={column.field}>{row[column.field]}</TableCell>
+                  <EditableTableCell
+                    key={column.field}
+                    value={row[column.field]}
+                    rowIndex={rowIndex}
+                    field={column.field}
+                    onCellValueChange={handleCellValueChange}
+                  />
                 ))}
               </TableRow>
             ))}
@@ -101,6 +114,49 @@ export const TableRenderer = ({ sheetData }) => {
   );
 };
 
+function EditableTableCell({ value, rowIndex, field, onCellValueChange }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [cellValue, setCellValue] = useState(value);
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    if (cellValue !== value) {
+      onCellValueChange(cellValue, rowIndex, field);
+    }
+  };
+
+  const handleChange = (event) => {
+    setCellValue(event.target.value);
+  };
+
+  return (
+    <TableCell onDoubleClick={handleDoubleClick}>
+      {isEditing ? (
+        <input
+          type="text"
+          value={cellValue}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          autoFocus
+        />
+      ) : (
+        value
+      )}
+    </TableCell>
+  );
+}
+
+EditableTableCell.propTypes = {
+  value: PropTypes.any.isRequired,
+  rowIndex: PropTypes.number.isRequired,
+  field: PropTypes.string.isRequired,
+  onCellValueChange: PropTypes.func.isRequired
+};
+
 function TablePaginationActions(props) {
   const theme = useTheme();
   const { count, page, rowsPerPage, onPageChange } = props;
@@ -119,7 +175,7 @@ function TablePaginationActions(props) {
 
   const handleLastPageButtonClick = (event) => {
     onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };  
+  };
 
   return (
     <Box sx={{ flexShrink: 0, ml: 2.5 }}>
@@ -128,28 +184,36 @@ function TablePaginationActions(props) {
         disabled={page === 0}
         aria-label="first page"
       >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
       <IconButton
         onClick={handleBackButtonClick}
         disabled={page === 0}
         aria-label="previous page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="last page"
       >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
     </Box>
   );
@@ -159,5 +223,5 @@ TablePaginationActions.propTypes = {
   count: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired,
   page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired
 };
