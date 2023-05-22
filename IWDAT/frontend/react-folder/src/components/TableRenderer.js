d9ffdb7fed13f5a398403f299feeec279e906210
Boey01 <boeyshuwei@gmail.com>
2023-05-22 21:58:10 +0800

    Adding pagination

--- IWDAT/frontend/react-folder/src/components/TableRenderer.js
+++ IWDAT/frontend/react-folder/src/components/TableRenderer.js
@@ -1,17 +1,18 @@
-import React from "react"
-import TableContainer from '@mui/material/TableContainer';
-import Table from '@mui/material/Table';
-import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+import React, { useState } from "react";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import TablePagination from "@mui/material/TablePagination";
 
 const CustomTableContainer = styled(TableContainer)({
-  maxHeight: '60vh',
-  overflowY: 'auto',
-  width: '70vw',
+  maxHeight: "60vh",
+  overflowY: "auto",
+  width: "70vw"
 });
 
 export const TableRenderer = ({ sheetData }) => {
@@ -26,11 +27,25 @@ export const TableRenderer = ({ sheetData }) => {
   Object.keys(firstRow).forEach((key) => {
     columns.push({
       field: key,
-      headerName: key,
+      headerName: key
     });
   });
 
   const rows = sheetData[sheetName[0]];
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
     <div>
@@ -44,16 +59,35 @@ export const TableRenderer = ({ sheetData }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row, index) => (
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row, index) => (
               <TableRow key={index}>
                 {columns.map((column) => (
                   <TableCell key={column.field}>{row[column.field]}</TableCell>
                 ))}
               </TableRow>
             ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={columns.length} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </CustomTableContainer>
+
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25,50]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </div>
   );
 };
