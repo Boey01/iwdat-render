acd7486ceb04cd38ca75d8af088b193dd13b8c30
Boey01 <boeyshuwei@gmail.com>
2023-05-24 09:27:08 +0800

    simple drawer

--- IWDAT/frontend/react-folder/src/components/DataRenderMain.js
+++ IWDAT/frontend/react-folder/src/components/DataRenderMain.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FileImport } from "./FileUploadHandler";
 import { TableRenderer } from "./TableRenderer";
 import Grid from '@mui/material/Grid';
+import MiniDrawer from "./SideBar";
 
 export const RendererMain = () => {
   const [sheetData, setSheetData] = useState(null);
@@ -14,6 +15,8 @@ export const RendererMain = () => {
   };
 
   return (
+    <>
+    <MiniDrawer/>
     <Grid container spacing={1} className="content">
       <Grid item xs={12} className="file-upload-section">
         <FileImport onFileUploaded={(e) => handleFileUploaded(e)} />
@@ -27,5 +30,6 @@ export const RendererMain = () => {
         </div>
       )}
     </Grid>
+    </>
   );
 };
