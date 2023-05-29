92d093ef0450bed0c9571b43049ff6eefd80efdb
Boey01 <boeyshuwei@gmail.com>
2023-05-29 20:58:44 +0800

    Fixed editable cell 1st page bug

--- IWDAT/frontend/react-folder/src/components/Tables/TableRenderer.js
+++ IWDAT/frontend/react-folder/src/components/Tables/TableRenderer.js
@@ -6,21 +6,15 @@
   import TableRow from "@mui/material/TableRow";
   import TableCell from "@mui/material/TableCell";
   import Paper from "@mui/material/Paper";
-  import { styled, useTheme } from "@mui/material/styles";
+  import { styled} from "@mui/material/styles";
   import TablePagination from "@mui/material/TablePagination";
-  import IconButton from "@mui/material/IconButton";
-  import FirstPageIcon from "@mui/icons-material/FirstPage";
-  import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-  import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-  import LastPageIcon from "@mui/icons-material/LastPage";
-  import PropTypes from "prop-types";
-  import Box from "@mui/material/Box";
   import Popover from "@mui/material/Popover";
   import FormGroup from "@mui/material/FormGroup";
   import FormControlLabel from "@mui/material/FormControlLabel";
   import Checkbox from "@mui/material/Checkbox";
   import Button from "@mui/material/Button";
   import EditableTableCell from "./EditableTableCell"
+  import TablePaginationActions from "./TablePaginationActions"
 
   const CustomTableContainer = styled(TableContainer)({
     maxHeight: "60vh",
@@ -179,6 +173,8 @@
                           rowIndex={rowIndex}
                           field={column.field}
                           onCellValueChange={handleCellValueChange}
+                          page = {page}
+                          rowsPerPage={rowsPerPage}
                         />
                       ))}
                   </TableRow>
@@ -207,71 +203,4 @@
     );
   }
 
-  function TablePaginationActions(props) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-      onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-      onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-      onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-        <IconButton
-          onClick={handleFirstPageButtonClick}
-          disabled={page === 0}
-          aria-label="first page"
-        >
-          {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-        </IconButton>
-        <IconButton
-          onClick={handleBackButtonClick}
-          disabled={page === 0}
-          aria-label="previous page"
-        >
-          {theme.direction === "rtl" ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-        </IconButton>
-        <IconButton
-          onClick={handleNextButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="next page"
-        >
-          {theme.direction === "rtl" ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </IconButton>
-        <IconButton
-          onClick={handleLastPageButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="last page"
-        >
-          {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-        </IconButton>
-      </Box>
-    );
-  }
-
-  TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-  };
+  
