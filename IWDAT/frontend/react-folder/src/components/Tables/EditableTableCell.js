aea402a28dbbac5677b7d31c94324e3506992ecc
Boey01 <boeyshuwei@gmail.com>
2023-05-29 20:58:50 +0800

    Fixed editable cell 1st page bug

--- IWDAT/frontend/react-folder/src/components/Tables/EditableTableCell.js
+++ IWDAT/frontend/react-folder/src/components/Tables/EditableTableCell.js
@@ -1,53 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TableCell from "@mui/material/TableCell";
 import PropTypes from "prop-types";
 
-export default function EditableTableCell({ value, rowIndex, field, onCellValueChange }) {
-    const [isEditing, setIsEditing] = useState(false);
-    const [cellValue, setCellValue] = useState(value);
-  
-    const handleDoubleClick = () => {
-      setIsEditing(true);
-    };
-  
-    const handleBlur = () => {
-      setIsEditing(false);
-      if (cellValue !== value) {
-        onCellValueChange(cellValue, rowIndex, field);
-      }
-    };
-  
-    const handleKeyDown = (event) => {
-      if (event.key === "Enter") {
-        handleBlur();
-      }
-    };
-  
-    const handleChange = (event) => {
-      setCellValue(event.target.value);
-    };
-  
-    return (
-      <TableCell onDoubleClick={handleDoubleClick}>
-        {isEditing ? (
-          <input
-            type="text"
-            value={cellValue}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            onKeyDown={handleKeyDown}
-            autoFocus
-          />
-        ) : (
-          value
-        )}
-      </TableCell>
-    );
-  }
-  
-  EditableTableCell.propTypes = {
-    value: PropTypes.any.isRequired,
-    rowIndex: PropTypes.number.isRequired,
-    field: PropTypes.string.isRequired,
-    onCellValueChange: PropTypes.func.isRequired,
+function EditableTableCell({
+  value,
+  rowIndex,
+  field,
+  onCellValueChange,
+  page ,
+  rowsPerPage,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [cellValue, setCellValue] = useState(value);
+
+  useEffect(() => {
+    setCellValue(value);
+  }, [value]);
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
   };
+
+ const handleBlur = () => {
+  setIsEditing(false);
+  if (cellValue !== value) {
+    const actualRowIndex = page * rowsPerPage + rowIndex;
+    onCellValueChange(cellValue, actualRowIndex, field);
+  }
+};
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleBlur();
+    }
+  };
+
+  const handleChange = (event) => {
+    setCellValue(event.target.value);
+  };
+
+  return (
+    <TableCell onDoubleClick={handleDoubleClick}>
+      {isEditing ? (
+        <input
+          type="text"
+          value={cellValue}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+      ) : (
+        value
+      )}
+    </TableCell>
+  );
+}
+
+EditableTableCell.propTypes = {
+  value: PropTypes.any.isRequired,
+  rowIndex: PropTypes.number.isRequired,
+  field: PropTypes.string.isRequired,
+  onCellValueChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired
+};
+
+export default React.memo(EditableTableCell);
