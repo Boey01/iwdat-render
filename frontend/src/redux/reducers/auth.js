32df5e312c2761e3e05b4e1b48a79bb15baf2f37
Boey01 <boeyshuwei@gmail.com>
2023-06-24 17:20:10 +0800

    backup before npm packages change

--- frontend/src/redux/reducers/auth.js
+++ frontend/src/redux/reducers/auth.js
@@ -12,7 +12,7 @@ const initialState = {
     user: null
 };
 
-export default function at(state = initialState, action){
+export default function authReducer(state = initialState, action){
     const {type, payload} = action;
 
     switch(type){
---
+++ frontend/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'react-redux';
+import authReducer from './auth';
+
+export default combineReducers({
+    authReducer
+})
