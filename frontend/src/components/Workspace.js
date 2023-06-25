a3f9a984bae332d1d951a9e5c383bb279d8f1ca2
Boey01 <boeyshuwei@gmail.com>
2023-06-26 00:33:03 +0800

    append export tables into global table list

--- frontend/src/components/Workspace.js
+++ frontend/src/components/Workspace.js
@@ -2,15 +2,18 @@ import React from 'react';
 import MiniDrawer from './SideBar';
 import ZIndexProvider from './ZIndexContext';
 import TableManager from './DataImporting/TableManager';
+import GlobalTablesProvider from '. /contexts/TableContext'
 
 export const RendererMain = () => {
 
   return (
+    <GlobalTablesProvider>
     <ZIndexProvider>
       <MiniDrawer />
       <TableManager/>
       <div class= "content">
         </div>
     </ZIndexProvider>
+    </GlobalTablesProvider>
   );
 };
