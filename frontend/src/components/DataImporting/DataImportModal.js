a95e1a1b9702d4ed7433db7208b2caba41218d16
Boey01 <boeyshuwei@gmail.com>
2023-07-01 20:51:57 +0800

    Splitted data import modal from table manager.

---
+++ frontend/src/components/DataImporting/DataImportModal.js
@@ -0,0 +1,146 @@
+import React, { useState} from "react";
+import FileImport from "./FileUploadHandler";
+import Grid from "@mui/material/Grid";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Box } from "@mui/material";
+import PreviewTable from "./TablePreview";
+import Input from '@mui/material/Input';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+
+export default function DIModalContent({
+    uploadedFile,
+     setUploadedFile,
+     handleCloseModal,
+     addTablesToGlobalTableList
+}) {
+    const [currentTab, setcurrentTab] = useState(0);
+    const [checkedTables, setCheckedTables] = useState([]);
+    const [tableNames, setTableNames] = useState([]);
+    
+    const handleChange = (event, newValue) => {
+        setcurrentTab(newValue);
+      };
+
+    const handleFileUploaded = (fileData) => {
+        // const newFiles = [...uploadedFiles, fileData];
+        setUploadedFile(fileData);
+        setCheckedTables([]);
+        setTableNames([]);
+      };
+
+    const handleCheckboxChange = (event) => {
+        const { id, checked } = event.target;
+        const index = id;
+        setCheckedTables((prevcheckedTables) => ({
+          ...prevcheckedTables,
+          [index]: checked,
+        }));
+    
+      };
+    
+      const handleTableNameChange = (index, value) => {
+      const updatedTableNames = [...tableNames];
+      updatedTableNames[index] = value;
+      setTableNames(updatedTableNames);
+    };
+    
+    const handleConfirmTables = () => {
+      const selectedTables = {};
+      for (const key in checkedTables) {
+        if (checkedTables[key]) {
+          const tableName = tableNames[key] || Object.keys(uploadedFile)[key];
+          selectedTables[tableName] = uploadedFile[Object.keys(uploadedFile)[key]];
+        }
+      }
+    
+      if(Object.keys(selectedTables).length !== 0){
+        addTablesToGlobalTableList(selectedTables);
+    }
+      // Reset the checked tables and close the popover/modal if needed
+      setCheckedTables([]);
+      handleCloseModal();
+      setTableNames([]);
+      setUploadedFile(null);
+    };
+
+    const ModalContent = styled("div")({
+        position: "absolute",
+        top: "50%", 
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        backgroundColor: "white",
+        padding: "16px",
+      });
+
+
+    function TabPanel(props) {
+        const { children, value, index } = props;
+        return (
+          <div role="tabpanel" hidden={value !== index}>
+            {value === index && <Box sx={{ p: 2 }}>{children}</Box>}
+          </div>
+        );
+      }
+
+    return(
+        <ModalContent className="table-preview-modal-content">
+        <Button
+          onClick={handleCloseModal}
+          style={{ position: "absolute", top: "8px", right: "8px" }}
+        >
+          X
+        </Button>
+        <Grid container spacing={1}>
+<Grid item xs={12} className="file-upload-section">
+  <FileImport onFileUploaded={handleFileUploaded} />
+</Grid>
+{uploadedFile && (
+  <>
+    <Grid item xs={12}>
+      <Tabs
+        value={currentTab}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="primary"
+  >
+        {Object.keys(uploadedFile).map((key, index) => (
+          <Tab label={key} index={index} />
+        ))}
+      </Tabs>
+    </Grid>
+    <Grid item xs={12}>
+      {Object.keys(uploadedFile).map((key, index) => (
+        <TabPanel value={currentTab} index={index} key={index}>
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item>
+              <Checkbox
+                id={index}
+                checked={checkedTables[index] || false}
+                onChange={handleCheckboxChange}
+              />
+            </Grid>
+            <Grid item>
+            <Input
+            defaultValue={tableNames[index] || key}
+            onBlur={(event) => handleTableNameChange(index, event.target.value)}
+            inputProps={{ 'aria-label': 'Table name' }}
+            />
+            </Grid>
+          </Grid>
+          <PreviewTable data={uploadedFile[key]} />
+        </TabPanel>
+      ))}
+    </Grid>
+    <Grid item xs={12}>
+      <Button variant="outlined" onClick={handleConfirmTables}>Confirm Tables</Button>
+    </Grid>
+  </>
+)}
+</Grid>
+
+      </ModalContent>
+    )
+}
--- frontend/src/components/DataImporting/TableManager.js
+++ frontend/src/components/DataImporting/TableManager.js
@@ -3,8 +3,6 @@ import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import MakeDraggable from "../Draggable";
 import Modal from "@mui/material/Modal";
-import FileImport from "./FileUploadHandler";
-import Grid from "@mui/material/Grid";
 import TableRenderer from "../Table/TableRenderer";
 import Popover from "@mui/material/Popover";
 import List from "@mui/material/List";
@@ -13,40 +11,20 @@ import ListItemButton from "@mui/material/ListItemButton";
 import Checkbox from "@mui/material/Checkbox";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { Box } from "@mui/material";
-import PreviewTable from "./TablePreview";
-import Input from '@mui/material/Input';
 import { GlobalTableContext } from "../contexts/TableContext";
+import DIModalContent from "./DataImportModal";
 
 const TableManagerButton = styled(Button)({
   left: "10vw",
 });
 
-const ModalContent = styled("div")({
-  position: "absolute",
-  top: "50%", 
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "white",
-  padding: "16px",
-});
-
 export default function TableManager() {
-  const { globalTables, setGlobalTables, addTablesToGlobalTableList, deleteGlobalTable } = useContext(GlobalTableContext);
+  const { globalTables, addTablesToGlobalTableList, deleteGlobalTable } = useContext(GlobalTableContext);
   const [modalOpen, setModalOpen] = useState(false);
   const [popoverOpen, setPopoverOpen] = useState(false);
   const [uploadedFile, setUploadedFile] = useState(null);
   // const [uploadedFiles, setUploadedFiles] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [currentTab, setcurrentTab] = useState(0);
-  const [checkedTables, setCheckedTables] = useState([]);
-  const [tableNames, setTableNames] = useState([]);
-
-  const handleChange = (event, newValue) => {
-    setcurrentTab(newValue);
-  };
 
   const handleOpenModal = () => {
     setModalOpen(true);
@@ -55,13 +33,6 @@ export default function TableManager() {
   const handleCloseModal = () => {
     setModalOpen(false);
     setUploadedFile(null);
-  };
-
-  const handleFileUploaded = (fileData) => {
-    // const newFiles = [...uploadedFiles, fileData];
-    setUploadedFile(fileData);
-    setCheckedTables([]);
-    setTableNames([]);
   };
 
   const handleOpenPopover = (event) => {
@@ -76,51 +47,6 @@ export default function TableManager() {
     const handleDeleteTable = (index) => {
       deleteGlobalTable(index);
     };
-
-  const handleCheckboxChange = (event) => {
-    const { id, checked } = event.target;
-    const index = id;
-    setCheckedTables((prevcheckedTables) => ({
-      ...prevcheckedTables,
-      [index]: checked,
-    }));
-
-  };
-
-  const handleTableNameChange = (index, value) => {
-  const updatedTableNames = [...tableNames];
-  updatedTableNames[index] = value;
-  setTableNames(updatedTableNames);
-};
-
-const handleConfirmTables = () => {
-  const selectedTables = {};
-  for (const key in checkedTables) {
-    if (checkedTables[key]) {
-      const tableName = tableNames[key] || Object.keys(uploadedFile)[key];
-      selectedTables[tableName] = uploadedFile[Object.keys(uploadedFile)[key]];
-    }
-  }
-
-  if(Object.keys(selectedTables).length !== 0){
-    addTablesToGlobalTableList(selectedTables);
-}
-  // Reset the checked tables and close the popover/modal if needed
-  setCheckedTables([]);
-  setPopoverOpen(false);
-  handleCloseModal();
-  setTableNames([]);
-  setUploadedFile(null);
-};
-
-  function TabPanel(props) {
-    const { children, value, index } = props;
-    return (
-      <div role="tabpanel" hidden={value !== index}>
-        {value === index && <Box sx={{ p: 2 }}>{children}</Box>}
-      </div>
-    );
-  }
 
   return (
     <>
@@ -175,62 +101,12 @@ const handleConfirmTables = () => {
       </Popover>
 
       <Modal open={modalOpen} onClose={handleCloseModal}>
-        <ModalContent className="table-preview-modal-content">
-          <Button
-            onClick={handleCloseModal}
-            style={{ position: "absolute", top: "8px", right: "8px" }}
-          >
-            X
-          </Button>
-          <Grid container spacing={1}>
-  <Grid item xs={12} className="file-upload-section">
-    <FileImport onFileUploaded={handleFileUploaded} />
-  </Grid>
-  {uploadedFile && (
-    <>
-      <Grid item xs={12}>
-        <Tabs
-          value={currentTab}
-          onChange={handleChange}
-          textColor="secondary"
-          indicatorColor="primary"
-    >
-          {Object.keys(uploadedFile).map((key, index) => (
-            <Tab label={key} index={index} />
-          ))}
-        </Tabs>
-      </Grid>
-      <Grid item xs={12}>
-        {Object.keys(uploadedFile).map((key, index) => (
-          <TabPanel value={currentTab} index={index} key={index}>
-            <Grid container alignItems="center" spacing={2}>
-              <Grid item>
-                <Checkbox
-                  id={index}
-                  checked={checkedTables[index] || false}
-                  onChange={handleCheckboxChange}
-                />
-              </Grid>
-              <Grid item>
-              <Input
-              defaultValue={tableNames[index] || key}
-              onBlur={(event) => handleTableNameChange(index, event.target.value)}
-              inputProps={{ 'aria-label': 'Table name' }}
-              />
-              </Grid>
-            </Grid>
-            <PreviewTable data={uploadedFile[key]} />
-          </TabPanel>
-        ))}
-      </Grid>
-      <Grid item xs={12}>
-        <Button variant="outlined" onClick={handleConfirmTables}>Confirm Tables</Button>
-      </Grid>
-    </>
-  )}
-</Grid>
-
-        </ModalContent>
+            <DIModalContent
+            uploadedFile={uploadedFile}
+            setUploadedFile={setUploadedFile}
+            handleCloseModal={handleCloseModal}
+            addTablesToGlobalTableList={addTablesToGlobalTableList}
+            />
       </Modal>
 
       <div className="table-workspace">
