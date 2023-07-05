25862e2149b0814c9bbcc4920a3203ddb01681e0
Boey01 <boeyshuwei@gmail.com>
2023-07-05 23:09:44 +0800

    backup before fixing the checking glitch

--- frontend/src/components/TableDataUtil/CustomizedAccordians.js
+++ frontend/src/components/TableDataUtil/CustomizedAccordians.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { styled } from '@mui/material/styles';
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion from '@mui/material/Accordion';
 import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import { Checkbox, List, ListItem, ListItemText } from '@mui/material';
+import { Checkbox, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import Divider from "@mui/material/Divider";
 
 const Accordion = styled((props) => (
@@ -44,38 +44,64 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
-export default function CustomizedAccordions({ tableList }) {
+export default function CustomizedAccordions({ tableList, mergeTables}) {
   const [expanded, setExpanded] = useState(null);
+  const [selectedRowCount, setSelectedRowCount] = useState(null);
+  const [selectedColumns, setSelectedColumns] = useState([]);
 
   const handleChange = (index) => (event, newExpanded) => {
     setExpanded(newExpanded ? index : false);
   };
 
+  const handleColumnSelected = (index, column) => (event) => {
+    const selectedTableData = tableList[index].data;
+    setSelectedRowCount(selectedTableData.length);
+
+    if (event.target.checked) {
+      setSelectedColumns([...selectedColumns, { index: index, columnName: column }]);
+    } else {
+      setSelectedColumns(selectedColumns.filter((selectedColumn) => selectedColumn.columnName !== column));
+    }
+  };
+
+  useEffect(() => {
+    if (selectedColumns.length === 0) {
+      setSelectedRowCount(null);
+    } else {
+      mergeTables(selectedColumns);
+    }
+  }, [selectedColumns, mergeTables]);
+  
   return (
     <>
-    <div className='aslkdjgf'>
       {tableList.map((data, index) => (
-        <Accordion expanded={expanded === index} onChange={handleChange(index)} key={index}>
+        <Accordion
+          expanded={expanded === index}
+          onChange={handleChange(index)}
+          key={index}
+          disabled={ selectedRowCount !== null && data.data.length !== selectedRowCount}
+        >
           <AccordionSummary>
-            <Typography>{data.name}</Typography>
+            <Typography>{data.name} ({data.data.length} rows)</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <List sx={{p:0}}>
+            <List disablePadding>
               {Object.keys(data['data'][0]).map((key) => (
-                <>
-                <ListItem key={key} sx={{p:0}}>
-                  <Checkbox />
-                  <ListItemText primary={key} p/>
-                  
-                </ListItem>
-                <Divider/>
-                </>
+                <React.Fragment key={key}>
+                  <ListItem disablePadding>
+                    <ListItemButton sx={{ p: 0 }}>
+                      <Checkbox onChange={handleColumnSelected(index, key)} />
+                      <ListItemText primary={key} p />
+                    </ListItemButton>
+                  </ListItem>
+                  <Divider />
+                </React.Fragment>
               ))}
             </List>
           </AccordionDetails>
         </Accordion>
       ))}
-      </div>
     </>
   );
 }
+
--- frontend/src/components/TableDataUtil/TableBindModal.js
+++ frontend/src/components/TableDataUtil/TableBindModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect,useCallback } from "react";
 import Grid from "@mui/material/Grid";
 import { Box, Card, Paper } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -8,6 +8,25 @@ import Typography from "@mui/material/Typography";
 import CustomizedAccordions from "./CustomizedAccordians";
 
 export default function TBModalContent({ handleCloseModal, globalTables }) {
+  const [mergedData, setMergedData] = useState([]);
+
+  const mergeTables = useCallback((selectedColumns) => {
+    const newMergedTables = selectedColumns.map(({ index, columnName }) => {
+      const selectedTableData = globalTables[index].data;
+      const mergedTable = selectedTableData.map((row) => ({
+        [columnName]: row[columnName],
+      }));
+      return mergedTable;
+    });
+  
+    setMergedData(newMergedTables);
+  
+  }, []); 
+
+  useEffect(() => {
+    console.log(mergedData)
+  }, [mergedData]);
+
   const ModalContent = styled("div")({
     position: "absolute",
     top: "50%",
@@ -21,7 +40,7 @@ export default function TBModalContent({ handleCloseModal, globalTables }) {
   return (
     <ModalContent className="table-preview-modal-content">
       <Button
-        onClick={handleCloseModal}
+        onClick={()=>(handleCloseModal(1))}
         style={{ position: "absolute", top: "8px", right: "8px" }}
       >
         X
@@ -29,21 +48,22 @@ export default function TBModalContent({ handleCloseModal, globalTables }) {
 
       <Grid container spacing={1}>
         <Grid item xs={3}>
-          <Paper variant="outlined" sx={{height:"55vh"}}>
-            <Card sx={{ p: 1, height:"12%" }}>
-              <Typography>
-                Please select the columns from exported tables.
-              </Typography>
+          <Paper variant="outlined" sx={{ height: "55vh" }}>
+            <Card sx={{ p: 2, height: "auto", textAlign: "center" }}>
+              <Typography>Exported Tables</Typography>
             </Card>
-            <Box sx={{ overflow: "auto", height:"88%"}}>
-              <CustomizedAccordions tableList={globalTables} />
+            <Box sx={{ overflow: "auto", height: "89%" }}>
+              <CustomizedAccordions
+                tableList={globalTables}
+                mergeTables={mergeTables}
+              />
             </Box>
           </Paper>
         </Grid>
 
         <Grid item xs={9}>
-            <Typography variant="h4">Table Preview:</Typography>
-            
+          <Typography variant="h4">Table Preview:</Typography>
+
         </Grid>
       </Grid>
     </ModalContent>
