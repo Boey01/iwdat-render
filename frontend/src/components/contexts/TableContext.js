eca779eb033069f9b4c9f295a16894810302378c
Boey01 <boeyshuwei@gmail.com>
2023-07-08 16:24:01 +0800

    update visiibility and position to backend done

--- frontend/src/components/contexts/TableContext.js
+++ frontend/src/components/contexts/TableContext.js
@@ -1,29 +1,64 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useCallback, useRef } from 'react';
 import { connect } from 'react-redux';
 import axios from "axios";
 import { Snackbar } from '@mui/material';
+import { debounce } from 'lodash';
 
 export const GlobalTableContext = createContext();
 
 export function GlobalTablesProvider({ children, isAuthenticated }) {
   const [globalTables, setGlobalTables] = useState([]);
-  const [saveState, setSaveState] = useState(0); //0 = saved, 1= need save, 2= saving
+  const [saveState, setSaveState] = useState(0); //0 = saved, 1= need save, 2= saving, 3= need save for visiblity and position(online)
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMsg, setSnackbarMsg] = useState(null);
+  const [movedTables, setMovedTables] = useState({});
+  const [hiddenTables, setHiddenTables] = useState({});
+
+  const hiddenTablesRef = useRef(hiddenTables);
+  const movedTablesRef = useRef(movedTables);
+
+  let positionChangeTimer;
+  let hiddenChangeTimer;
+
+  const updateTablesVisibilityDebounce = useCallback(
+    debounce(() => {
+      updateTablesVisibility(hiddenTablesRef.current);
+    }, 3000),
+    []
+  );
+
+  const updateTablesPositionDebounce = useCallback(
+    debounce(() => {
+      updateTablesPosition(movedTablesRef.current);
+    }, 3000),
+    []
+  );
 
   useEffect(() => {
     loadAccountTables();
-    // const storedGlobalTables = localStorage.getItem('globalTables');
-
-    // if (storedGlobalTables) {
-    //   setGlobalTables(JSON.parse(storedGlobalTables));
-    // }
+    if(!isAuthenticated){
+    if (localStorage.getItem('globalTables')) {
+      setGlobalTables(JSON.parse(localStorage.getItem('globalTables')));
+    }}
+    return () => {
+      updateTablesVisibilityDebounce.cancel();
+      updateTablesPositionDebounce.cancel();
+    };
   }, []);
+
+  useEffect(() => {
+    hiddenTablesRef.current = hiddenTables;
+  }, [hiddenTables]);
+
+  useEffect(() => {
+    movedTablesRef.current = movedTables;
+  }, [movedTables]);
 
   const handleSnackbarClose = () =>{
     setSnackbarOpen(false);
   };
 
+  // Add tables ----------------------------------------------------\
   const addTablesToGlobalTableList = (tables) => {
     if (isAuthenticated) {
       Object.keys(tables).map((key) => {
@@ -51,38 +86,69 @@ export function GlobalTablesProvider({ children, isAuthenticated }) {
     }
   };
 
+   // -------------------------------------------------------------/
+
+  // Delete tables -----------------------------------------------\
   const deleteGlobalTable = (index) => {
   if(isAuthenticated){
     const targetTableID = globalTables[index]["table_id"];
      deleteTable(targetTableID, index);
   }else{
-    deleteFromTableList (index);
+    deleteFromTableListUseState (index);
+    setSaveState(1);
   }
   };
 
-  const deleteFromTableList = (index) =>{
+  const deleteFromTableListUseState = (index) =>{
     const updatedTables = [...globalTables];
     updatedTables.splice(index, 1);
     setGlobalTables(updatedTables);
-    // setSaveState(1);
   }
+  // -------------------------------------------------------------/
 
+    // Edit tables -----------------------------------------------\
   const toggleTableVisibility = (index) => {
+    
     setGlobalTables((prevTables) => {
       const updatedTables = [...prevTables];
       updatedTables[index].hidden = !updatedTables[index].hidden;
       return updatedTables;
     });
-    // setSaveState(1);
-  };
 
-  const updateTablePosition = (index, newPosition) => {
+    if (isAuthenticated) {
+      clearTimeout(hiddenChangeTimer);
+
+     
+      hiddenTables[globalTables[index].table_id]= globalTables[index].hidden;
+      
+      hiddenChangeTimer = setTimeout(() => {
+        updateTablesVisibilityDebounce();
+      },3000);
+    }else{
+      setSaveState(1);
+    }
+  };
+  
+
+  const updateTablePosition = (index, x, y) => {
     setGlobalTables((prevTables) => {
       const updatedTables = [...prevTables];
-      updatedTables[index].position = newPosition;
+      updatedTables[index].position_x = x;
+      updatedTables[index].position_y = y;
       return updatedTables;
     });
-    // setSaveState(1);
+
+    if (isAuthenticated) {
+      clearTimeout(positionChangeTimer);
+
+      movedTables[globalTables[index].table_id] = globalTables[index].position_x +","+ globalTables[index].position_y;
+      
+      positionChangeTimer = setTimeout(() => {
+         updateTablesPositionDebounce();
+      },100);
+    }else{
+      setSaveState(1);
+    }
   };
 
   const updateTableDataEdit = (index, newData) =>{
@@ -106,86 +172,133 @@ export function GlobalTablesProvider({ children, isAuthenticated }) {
   };
   
 //--- BACKEND INVOVLED CRUD OPERATIONS ---------------------
+function addNewTableToAccount(table_name, position_x, position_y, hidden, data) {
+  if (localStorage.getItem("access")) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("access")}`,
+      },
+    };
 
-  const addNewTableToAccount = async (table_name, position_x, position_y, hidden, data ) => {
-    if (localStorage.getItem("access")) {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `JWT ${localStorage.getItem("access")}`,
-        },
-      };
+    const body = JSON.stringify({ table_name, position_x, position_y, hidden, data });
 
-      const body = JSON.stringify({ table_name, position_x, position_y, hidden, data });
-
-      try {
-        const response = await axios.post(
-          `${process.env.REACT_APP_BACKEND_API_URL}/tables/create/`,
-          body,
-      config
-        );
-          
-        if(response.status===200){
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_API_URL}/tables/create/`, body, config)
+      .then(function (response) {
+        if (response.status === 200) {
           const createdDatabaseTable = response.data;
-          setGlobalTables((prevTables) => [...prevTables, createdDatabaseTable]); 
-          }
-        }catch(err) {
-          console.log(err);
+          setGlobalTables(function (prevTables) {
+            return [...prevTables, createdDatabaseTable];
+          });
         }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
-  };
+}
 
+function loadAccountTables() {
+  if (localStorage.getItem("access")) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("access")}`,
+        Accept: "application/json",
+      },
+    };
 
-  const loadAccountTables = async () => {
-    if (localStorage.getItem("access")) {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `JWT ${localStorage.getItem("access")}`,
-          Accept: "application/json",
-        },
-      };
-  
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_API_URL}/tables/retrieve/`,
-          config
-        );
-
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_API_URL}/tables/retrieve/`, config)
+      .then(function (response) {
         setGlobalTables(response.data);
-      } catch (err) {
+      })
+      .catch(function (err) {
         console.log(err);
-      }
-    }
+      });
   }
+}
 
-  const deleteTable = async (table_id, indexToDelete) => {
-    if (localStorage.getItem("access")) {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `JWT ${localStorage.getItem("access")}`,
-        },
-      };
-  
-      try {
-        const response = await axios.delete(
-          `${process.env.REACT_APP_BACKEND_API_URL}/tables/delete/${table_id}/`,
-          config
-        );
-  
+function deleteTable(table_id, indexToDelete) {
+  if (localStorage.getItem("access")) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("access")}`,
+      },
+    };
+
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_API_URL}/tables/delete/${table_id}/`, config)
+      .then(function (response) {
         if (response.status === 204) {
-          setSnackbarMsg("Delete Succesful");
+          setSnackbarMsg("Delete Successful");
           setSnackbarOpen(true);
-          
-          deleteFromTableList (indexToDelete);
+
+          deleteFromTableListUseState(indexToDelete);
         }
-      } catch (err) {
+      })
+      .catch(function (err) {
         console.log(err);
-      }
-    }
-  };
-  
+      });
+  }
+}
+
+function updateTablesVisibility(refHiddenTable) {
+  if (localStorage.getItem("access")) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("access")}`,
+      },
+    };
+
+    const body = JSON.stringify(refHiddenTable);
+
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_API_URL}/tables/update/visible/`,
+        body,
+        config
+      )
+      .then(function (response) {
+        setHiddenTables({});
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  console.log(refHiddenTable);
+}
+
+function updateTablesPosition(refMovedTable) {
+  if (localStorage.getItem("access")) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.getItem("access")}`,
+      },
+    };
+
+    const body = JSON.stringify(refMovedTable);
+
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_API_URL}/tables/update/position/`,
+        body,
+        config
+      )
+      .then(function (response) {
+        setMovedTables({});
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  console.log(refMovedTable);
+}
+
   const updateAccountTableList = async () =>{
     if(isAuthenticated){
       console.log(globalTables);
