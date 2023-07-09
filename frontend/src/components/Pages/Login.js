6f43f0bfd5e60bfedb5c743f4012453ecb03a2c4
Boey01 <boeyshuwei@gmail.com>
2023-07-09 13:51:33 +0800

    Login password field with show & capslock indicator

--- frontend/src/components/Pages/Login.js
+++ frontend/src/components/Pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -10,89 +10,112 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { connect } from 'react-redux';
 import { redux_login } from "../../redux/actions/auth_actions";
-import {Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import KeyboardCapslockIcon from '@mui/icons-material/KeyboardCapslock';
 
-const Login = ({redux_login, isAuthenticated}) => {
+const Login = ({ redux_login, isAuthenticated }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [capsLockOn, setCapsLockOn] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.getModifierState("CapsLock")) {
+      setCapsLockOn(true);
+    } else {
+      setCapsLockOn(false);
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const email = data.get("email");
     const password = data.get("password");
-    redux_login(email,password);
+    redux_login(email, password);
   };
 
-  if(isAuthenticated){
-    return <Navigate to="/"/>
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
   }
 
   return (
     <div className="make-center">
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{  
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}>
-          
-        <Typography variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link to="/request/reset">
-                Forgot password?
-              </Link>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5">Sign in</Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              autoComplete="current-password"
+              InputProps={{
+                endAdornment: (
+                  <>
+                  {capsLockOn && <KeyboardCapslockIcon  edge="end"/>}
+                  <IconButton onClick={handleTogglePasswordVisibility} edge="end">
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                  </>
+                ),
+              }}
+              onKeyDown={handleKeyDown}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link to="/request/reset">Forgot password?</Link>
+              </Grid>
+              <Grid item>
+                <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
+              </Grid>
             </Grid>
-            <Grid item>
-            <Link to="/signup">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => ({
   isAuthenticated: state.authReducer.isAuthenticated,
 });
 
-export default connect(mapStateToProps, {redux_login})(Login);
+export default connect(mapStateToProps, { redux_login })(Login);
