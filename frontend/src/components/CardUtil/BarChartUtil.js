5ff6a36744f6f1eba736650e084207f06cbc8a94
Boey01 <boeyshuwei@gmail.com>
2023-07-13 14:57:44 +0800

    added color picker; changed scroll; change toggle

--- frontend/src/components/CardUtil/BarChartUtil.js
+++ frontend/src/components/CardUtil/BarChartUtil.js
@@ -5,18 +5,17 @@ import {
   AccordionSummary,
 } from "../util/CustomComponents";
 import {
-  FormControl,
   Grid,
-  InputLabel,
   MenuItem,
   Select,
   Stack,
   Typography,
   Button,
-  ToggleButton,
-  ToggleButtonGroup,
+  Switch,
+  FormControlLabel,
   Paper,
   Chip,
+  Popover,
 } from "@mui/material";
 import {
   BarChart,
@@ -31,22 +30,24 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
 import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
+import { ChromePicker } from "react-color";
 
 export default function BarChartPreview(data) {
   const [targetColumn, setTargetColumn] = useState("");
   const [valueColumns, setValueColumns] = useState([]);
   const [transformedData, setTransformedData] = useState([]);
   const [isGrouped, setIsGrouped] = useState(true); // Initially set to grouped
-  const [chartWidth, setChartWidth] = useState(100); 
-  const [chartHeight, setChartHeight] = useState(100); 
-  
-  useEffect(()=>{
-    console.log(transformedData); 
-  },[transformedData])
-  
-  useEffect(()=>{
-    console.log(valueColumns); 
-  },[valueColumns])
+  const [selectedBarKey, setSelectedBarKey] = useState("");
+  const [barColors, setBarColors] = useState({});
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  useEffect(() => {
+    console.log(transformedData);
+  }, [transformedData]);
+
+  useEffect(() => {
+    console.log(valueColumns);
+  }, [valueColumns]);
 
   const handleChange = (event) => {
     setTargetColumn(event.target.value);
@@ -69,23 +70,38 @@ export default function BarChartPreview(data) {
     setValueColumns(newColumns);
   };
 
+  function handleColorPickerOpen(event, key) {
+    setSelectedBarKey(key);
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleColorPickerClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleColorChange = (color) => {
+    setBarColors({ ...barColors, [selectedBarKey]: color.hex });
+  };
+
   const menuItems = Object.keys(data.data[0]).map((key) => (
-    <MenuItem value={key}>{key}</MenuItem>
+    <MenuItem value={key} key={key}>
+      {key}
+    </MenuItem>
   ));
 
   const transformData = () => {
     if (!targetColumn) {
       return [];
     }
-  
+
     const transformedData = data.data.reduce((result, item) => {
       const targetValue = item[targetColumn];
       const existingItem = result.find((d) => d[targetColumn] === targetValue);
-  
+
       if (existingItem) {
         valueColumns.forEach((column) => {
           const { columnName, type } = column;
-  
+
           if (type === "Direct Use") {
             existingItem[columnName] = item[columnName];
           } else if (type === "Count") {
@@ -104,10 +120,10 @@ export default function BarChartPreview(data) {
         });
       } else {
         const newDataItem = { [targetColumn]: targetValue };
-  
+
         valueColumns.forEach((column) => {
           const { columnName, type } = column;
-  
+
           if (type === "Direct Use") {
             newDataItem[columnName] = item[columnName];
           } else if (type === "Count") {
@@ -120,13 +136,13 @@ export default function BarChartPreview(data) {
             newDataItem[category] = 1;
           }
         });
-  
+
         result.push(newDataItem);
       }
-  
+
       return result;
     }, []);
-  
+
     if (isGrouped) {
       return transformedData;
     } else {
@@ -135,21 +151,22 @@ export default function BarChartPreview(data) {
         ...item,
       }));
     }
-  };  
+  };
 
   const handleApplyChanges = () => {
     const transformedData = transformData();
+    
+    // Generate random colors for each bar
+    const newBarColors = {};
+    Object.keys(transformedData[0])
+      .slice(1)
+      .forEach((key) => {
+        newBarColors[key] = barColors[key] || `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      });
+  
     setTransformedData(transformedData);
+    setBarColors(newBarColors); // Update barColors state with new colors
   };
-
-  const handleChartWidthChange = (event, newValue) => {
-    setChartWidth(newValue);
-  };
-
-  const handleChartHeightChange = (event, newValue) => {
-    setChartHeight(newValue);
-  };
-
   return (
     <>
       <Stack>
@@ -158,7 +175,17 @@ export default function BarChartPreview(data) {
             <Typography>Expand option</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Grid container spacing={2} sx={{mb:1, mt:0.5, overflow:"auto", maxHeight:"20vh"}}>
+            <Grid
+              container
+              spacing={2}
+              sx={{
+                mb: 1,
+                mt: 0.5,
+                pl: 1,
+                overflow: "auto",
+                maxHeight: "20vh",
+              }}
+            >
               {/* Row 1 */}
               <Grid item xs={12}>
                 <Typography>Target Column (Variable):</Typography>
@@ -179,23 +206,28 @@ export default function BarChartPreview(data) {
                 </Select>
               </Grid>
               <Grid item xs={3}>
-                  <ToggleButton 
-                  selected={isGrouped}
-                  onChange={() => {
-                    setIsGrouped(!isGrouped);
-                  }}
-                  sx={{ height: 35 }}
-                  >Group By </ToggleButton>
+              <FormControlLabel
+  control={
+    <Switch
+      checked={isGrouped}
+      onChange={() => {
+        setIsGrouped(!isGrouped);
+      }}
+      sx={{ height: 35 }}
+    />
+  }
+  label="Group By" // Replace "Group By" with your desired label text
+/>
               </Grid>
               <Grid item xs={4}></Grid>
 
               {/* Row 3 */}
               <Grid item xs={12}>
-              <Chip
+                <Chip
                   label="Add Value"
                   onClick={addNewValueColumn}
                   onDelete={addNewValueColumn}
-                  deleteIcon={ <AddIcon />}
+                  deleteIcon={<AddIcon />}
                 />
               </Grid>
 
@@ -235,7 +267,9 @@ export default function BarChartPreview(data) {
                       <MenuItem value={"Direct Use"}>Direct Use</MenuItem>
                       <MenuItem value={"Sum"}>Sum</MenuItem>
                       <MenuItem value={"Count"}>Count</MenuItem>
-                      <MenuItem value={"Categorical Count"}>Categorical Count</MenuItem>              
+                      <MenuItem value={"Categorical Count"}>
+                        Categorical Count
+                      </MenuItem>
                     </Select>
                   </Grid>
                   <Grid item xs={1}>
@@ -246,39 +280,89 @@ export default function BarChartPreview(data) {
                   <Grid item xs={3}></Grid>
                 </React.Fragment>
               ))}
-            {/* 4th Row */}   
-        </Grid>
-            {/* last Row */}              
 
-            <Button variant="contained" onClick={handleApplyChanges}>
-                  Apply Changes
-                </Button>
+              {/* 4th Row */}
+              {/* Color Picker Section */}
+              <Grid item xs={12}>
+                <Typography>Legends' Color:</Typography>
+                <Stack direction="row" spacing={1} sx={{ overflowX: "auto", p:0.5, border:"1px solid grey", borderRadius: 3 }}>
+                  {transformedData.length > 0 &&
+                    Object.keys(transformedData[0])
+                      .slice(1)
+                      .map((key) => (
+                        <Chip
+                          key={key}
+                          label={key}
+                          onClick={(e) => {
+                            handleColorPickerOpen(e, key);
+                          }}
+                          sx={{
+                            backgroundColor: barColors[key] || "grey",
+                            color: "#FFF",
+                            boxShadow: 2,
+                          }}
+                        />
+                      ))}
+                </Stack>
+              </Grid>
+            </Grid>
+
+            <Button
+              variant="contained"
+              onClick={handleApplyChanges}
+              sx={{ m: 1 }}
+            >
+              Apply Changes
+            </Button>
           </AccordionDetails>
         </Accordion>
       </Stack>
 
       {/* Render the Bar Chart */}
-      <Paper sx={{ overflow: "auto" }}>
-      {transformedData.length > 0 && (
-       <ResponsiveContainer width="100%" aspect={2}>
-      <BarChart data={transformedData}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey={targetColumn} />
-  <YAxis />
-  <Tooltip />
-  <Legend />
-  {Object.keys(transformedData[0]).slice(1).map((key) => (
-    <Bar
-      key={key}
-      dataKey={key}
-      fill={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
-    />
-  ))}
-</BarChart>
-</ResponsiveContainer>
- )}
-
+      <Paper sx={{ overflow: "auto", height: "30vh" }}>
+        {transformedData.length > 0 && (
+          <ResponsiveContainer width="100%" height="100%" >
+            <BarChart data={transformedData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={targetColumn} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              {Object.keys(transformedData[0])
+                .slice(1)
+                .map((key) => (
+                  <Bar
+                    key={key}
+                    dataKey={key}
+                    fill={
+                      barColors[key] 
+                    }
+                  />
+                ))}
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </Paper>
+
+      {/* Color Picker Dialog */}
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleColorPickerClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <ChromePicker
+          color={barColors[selectedBarKey] || "#000"}
+          onChange={handleColorChange}
+        />
+      </Popover>
     </>
   );
 }
