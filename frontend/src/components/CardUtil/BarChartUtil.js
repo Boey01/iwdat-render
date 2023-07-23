4eeefa83ec639fbad6a2498a22eb9e5754786ba1
Boey01 <boeyshuwei@gmail.com>
2023-07-23 22:56:12 +0800

    Backup checkpoint: transformingData

--- frontend/src/components/CardUtil/BarChartUtil.js
+++ frontend/src/components/CardUtil/BarChartUtil.js
@@ -27,6 +27,7 @@ import GridOnIcon from "@mui/icons-material/GridOn";
 import GridOffIcon from "@mui/icons-material/GridOff";
 import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
 import ToggleButton from "@mui/material/ToggleButton";
+import transformingData from "./transformData";
 
 export default function BarChartPreview({ data, defineVisualConfig }) {
   const [targetColumn, setTargetColumn] = useState("");
@@ -92,77 +93,13 @@ export default function BarChartPreview({ data, defineVisualConfig }) {
     </MenuItem>
   ));
 
-  const transformData = () => {
-    if (!targetColumn) {
-      return [];
-    }
-
-    const transformedData = data.reduce((result, item) => {
-      const targetValue = item[targetColumn];
-      const existingItem = result.find((d) => d[targetColumn] === targetValue);
-
-      if (existingItem) {
-        valueColumns.forEach((column) => {
-          const { columnName, type } = column;
-
-          if (type === "Direct Use") {
-            existingItem[columnName] = item[columnName];
-          } else if (type === "Count") {
-            existingItem[columnName]++;
-          } else if (type === "Sum") {
-            existingItem[columnName] += item[columnName];
-          } else if (type === "Categorical Count") {
-            // Create a separate bar for each category
-            const category = item[columnName];
-            if (!existingItem[category]) {
-              existingItem[category] = 1;
-            } else {
-              existingItem[category]++;
-            }
-          }
-        });
-      } else {
-        const newDataItem = { [targetColumn]: targetValue };
-
-        valueColumns.forEach((column) => {
-          const { columnName, type } = column;
-
-          if (type === "Direct Use") {
-            newDataItem[columnName] = item[columnName];
-          } else if (type === "Count") {
-            newDataItem[columnName] = 1;
-          } else if (type === "Sum") {
-            newDataItem[columnName] = item[columnName];
-          } else if (type === "Categorical Count") {
-            // Create a separate bar for each category
-            const category = item[columnName];
-            newDataItem[category] = 1;
-          }
-        });
-
-        result.push(newDataItem);
-      }
-
-      return result;
-    }, []);
-
-    if (isGrouped) {
-      return transformedData;
-    } else {
-      return data.map((item, index) => {
-        const newItem = { [targetColumn]: item[targetColumn] };
-        valueColumns.forEach((column) => {
-          const { columnName } = column;
-
-          newItem[columnName] = item[columnName];
-        });
-        return newItem;
-      });
-    }
-  };
-
   const handleApplyChanges = () => {
-    const transformedData = transformData();
+    const transformedData = transformingData(
+      data,
+      isGrouped,
+      targetColumn,
+      valueColumns
+    );
     console.log(transformedData);
     // Generate random colors for each bar
     const newBarColors = {};
